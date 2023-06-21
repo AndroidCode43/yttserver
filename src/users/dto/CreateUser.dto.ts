b1ecad00f91958b7be7c2ccc70b6803c427fec53
Dihiko <newdeveloper43@gmail.com>
2023-06-21 16:00:13 +0300

    Update CreateUser.dto.ts

--- src/users/dto/CreateUser.dto.ts
+++ src/users/dto/CreateUser.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, MinLength } from "class-validator";
+import { IsDateString, IsEmail, MinLength } from "class-validator";
 
 export class CreateUserDto{
 
@@ -7,7 +7,13 @@ export class CreateUserDto{
 
   @MinLength(8,{message: 'Пароль должен быть более 8 символов!'})
   readonly password: string;
+
+  @MinLength(3, {message: 'ФИО не должно содержать менее 5 символов!'})
   readonly name: string;
+
+  @MinLength(3, {message: 'Номер паспорта должен быть равен 10 символам!'})
   readonly passportNumber: string;
+
+  @IsDateString()
   readonly dob: string;
 }
